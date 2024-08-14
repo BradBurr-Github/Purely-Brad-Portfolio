31393150ad4ca8ec2be8b45f8294703392ce8177
unknown <bradburr@gmail.com>
2024-08-14 16:24:41 -0600

    rolled back my App.jsx to be a function

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,8 @@ import './App.css'
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 
-const App = () => {
-
-  return (
-      <div>
-        <section className="m-12 flex h-screen items-center justify-center bg-blue-300 text-center text-3xl p-12 md:p-14 xl:p-20">
-          <p>
-            Brad Burr - Full Stack Developer
-          </p>
-        </section>
-      </div>
-  )
+function App() {
+  return <Navbar />;
 }
 
 export default App
