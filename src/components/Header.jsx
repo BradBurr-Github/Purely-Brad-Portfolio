05104016c41bb9f90c045a5c85c2de259c01cfd5
unknown <bradburr@gmail.com>
2024-08-15 02:19:10 -0600

    flexbox formatting changes to the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,34 @@
 import { useState, useEffect } from 'react';
+import myPicture from '../../assets/images/myPicture.jpg';
 
 function Header() {
     return (  
-      <header className="font-[Poppins]">
-        <div className="heading-name">Brad Burr</div>
-        <div className="heading-title">Senior Software Engineer</div>
-        <ul className="header-links">
-            <li>
-            <a className="hover:text-gray-200" href="#">My Portfolio</a>
-            </li>
-            <li>
-            <a className="hover:text-gray-200" href="#">Experience</a>
-            </li>
-            <li>
-            <a className="hover:text-gray-200" href="#">About Me</a>
-            </li>
-            <li>
-            <a className="hover:text-gray-200" href="#">Contact Me</a>
-            </li>
-        </ul>
-      </header>
+        <header className="font-[Poppins] header">
+            <div className='heading-picture-name-title'>
+                <div>
+                    <img src={myPicture} alt="My Picture" className="heading-image"/>
+                </div>
+                <div>
+                    <div className="heading-name">Brad Burr</div>
+                    <div className="heading-title">Senior Software Engineer</div>
+                </div>
+            </div>
+            <ul className="header-links">
+                <li>
+                    <a className="hover:text-gray-200" href="#">My Portfolio</a>
+                </li>
+                <li>
+                    <a className="hover:text-gray-200" href="#">Experience</a>
+                </li>
+                <li>
+                    <a className="hover:text-gray-200" href="#">About Me</a>
+                </li>
+                <li>
+                    <a className="hover:text-gray-200" href="#">Contact Me</a>
+                </li>
+            </ul>
+        </header>
     );
-  }
+}
   
   export default Header;
