110ba348475d4a331130ee57b6709e0628a03bbd
unknown <bradburr@gmail.com>
2024-08-14 22:57:43 -0600

    modified body tag in CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,22 @@ import { useState, useEffect } from 'react';
 
 function Header() {
     return (  
-      <header class="font-[Poppins]">
+      <header className="font-[Poppins]">
         <h1>Brad Burr</h1>
+        <ul className="flex items-center gap-[4vw]">
+            <li>
+            <a className="hover:text-gray-50" href="#">My Portfolio</a>
+            </li>
+            <li>
+            <a className="hover:text-gray-50" href="#">Experience</a>
+            </li>
+            <li>
+            <a className="hover:text-gray-50" href="#">About Me</a>
+            </li>
+            <li>
+            <a className="hover:text-gray-50" href="#">Contact Me</a>
+            </li>
+        </ul>
       </header>
     );
   }
