1bf407a101e1caccc771bf9e6ba62c69b3278e60
unknown <bradburr@gmail.com>
2024-08-16 15:18:35 -0600

    added AboutMe to my App

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react';
 import Header from './components/Header';
 import Project from './components/Project';
 import Footer from './components/Footer';
+import AboutMe from './pages/AboutMe';
 
 function App() {
   return (
     <>
     <Header />
     <Project />
+    <AboutMe />
     <Footer />
     </>
   )
