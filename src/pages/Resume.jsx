4baf1c557cf06670540bb5143cb4009e5bdb87f4
unknown <bradburr@gmail.com>
2024-08-17 14:48:49 -0600

    updated Resume page with button and skills

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,37 +1,39 @@
 import Header from "../components/Header";
 
-const styles = {
-  button: {
-    padding: "10px",
-    background: 'green',
-    color: 'white',
-    border: '4px',
-    cursor: 'pointer',
-    font: '16px',
-    margin: '0px 0px 0px 20px',
-  },
-  mySkills: {
-    font: '36px',
-    padding: "50px"
-  }
-};
-
 export default function Resume() {
 
   return (
     <>
       <Header />
       <h1 className="pageTitle">My Resumé</h1>
-      <button style={styles.button} type="submit">Download PDF</button>
-      <p style={styles.mySkills}>My Skills
-        <ul>
-          <li>Javascript</li>
-          <li>C++</li>
-          <li>C#</li>
-          <li>CSS</li>
-          <li>HTML</li>
-        </ul>
-      </p>
+      <div className="resume-container">
+        <button className="resume-button" type="submit">Download PDF</button>
+        <p className="resume-mySkills">My Skills
+          <ul className="resume-listItems">
+            <li>Strong Work Ethic</li>
+            <li>Life-Long Student</li>
+            <li>Detail Oriented</li>
+            <li>Collaborative</li>
+            <li>C++</li>
+            <li>C#</li>
+            <li>HTML 5</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>Git</li>
+            <li>GraphQL</li>
+            <li>Postgres</li>
+            <li>MongoDB</li>
+            <li>Mongoose</li>
+            <li>Express Server</li>
+            <li>SQL Server</li>
+            <li>Node.js</li>
+            <li>jQuery</li>
+            <li>RESTful APIs</li>
+            <li>React JS</li>
+            <li>Full Stack MERN</li>
+          </ul>
+        </p>
+      </div>
     </>
   );
 }
