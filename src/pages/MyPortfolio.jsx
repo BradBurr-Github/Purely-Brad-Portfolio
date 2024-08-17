a03cdd539bbf229364fd5f3544bb395a7d4d104f
unknown <bradburr@gmail.com>
2024-08-16 22:31:15 -0600

    added 6 pictures of my projects and started to bring them into a card

--- src/pages/MyPortfolio.jsx
+++ src/pages/MyPortfolio.jsx
@@ -1,11 +1,28 @@
 import Header from "../components/Header";
+//import image from "../../assets/images"
 
 export default function MyPortfolio() {
 
   return (
     <>
       <Header />
-      <h2>My Portfolio</h2>
+      <div class="container">
+        <div class="cardContainer">
+          <article class="cardArticle">
+            <img src='' alt="card" className="cardImage"/>
+            <div class="cardData">
+              <span class="cardDesc"></span>
+              <h2 href="#" class="cardButton">Github Repository</h2>
+
+            </div>
+
+
+          </article>
+
+        </div>
+
+        
+      </div>
     </>
   );
 }
