5165dea683ee4234c1628c0898419c6b690de233
unknown <bradburr@gmail.com>
2024-08-17 23:50:23 -0600

    Footer was imported twice in ContactMe page

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,6 +1,5 @@
-import Footer from "../components/Footer";
 import Header from "../components/Header";
-import Contact from "../components/Contact"
+import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
 export default function ContactMe() {
