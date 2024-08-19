20654114175ef78bc4adfedb7b5aa2e55db38764
unknown <bradburr@gmail.com>
2024-08-19 04:01:06 -0600

    make current LINK active

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,31 +1,24 @@
-import { Link } from "react-router-dom";
-//import { useState } from 'react';
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 function Navigation() {
-  //const [currLink, setCurrLink] = useState('');
 
-  const handleOnClick = (e) => {
-    const { target } = e;
-    const navLinkPathname = new URL(target.href).pathname;
-    target.classList.add('active')
-    // setCurrLink(navLinkPathname)
-    // const navLinksEls = document.querySelectorAll('.nav-link');
-    // navLinksEls.forEach(navLinksEl => {
-    //   // Find correct element
-    //   const navLinkElementClicked = new URL(navLinksEl.href).pathname;
-    //   if (navLinkPathname===navLinkPathname2) {
-    //     navLinksEl.classList.add('active')
-    //     console.log(`1: ${navLinkPathname}   2: ${navLinkPathname2}`);
-    //   }
-    //});
-  }
+  // Set current link to Active
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState(location.pathname);
+  useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [activeLink]);
+  const handleOnClick = (pathname) => {
+    setActiveLink(pathname);
+  };
 
   return (
     <nav>
-      <Link className="nav-link" onClick={handleOnClick} to="/aboutme">About Me</Link>
-      <Link className="nav-link" onClick={handleOnClick} to="/myportfolio">My Portfolio</Link>
-      <Link className="nav-link" onClick={handleOnClick} to="/contactme">Contact Me</Link>
-      <Link className="nav-link" onClick={handleOnClick} to="/resume">Resumé</Link>
+      <Link className={`nav-link ${((activeLink === "/aboutme") || (activeLink === "/")) ? "active" : ""}`} onClick={() => handleOnClick("/aboutme")} to="/aboutme">About Me</Link>
+      <Link className={`nav-link ${activeLink === "/myportfolio" ? "active" : ""}`} onClick={() => handleOnClick("/myportfolio")} to="/myportfolio">My Portfolio</Link>
+      <Link className={`nav-link ${activeLink === "/contactme" ? "active" : ""}`} onClick={() => handleOnClick("/contactme")} to="/contactme">Contact Me</Link>
+      <Link className={`nav-link ${activeLink === "/resume" ? "active" : ""}`} onClick={() => handleOnClick("/resume")} to="/resume">Resumé</Link>
     </nav>
   );
 }
