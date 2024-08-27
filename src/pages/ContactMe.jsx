3942387a0142f1d8d6825cd86643dc30af840887
unknown <bradburr@gmail.com>
2024-08-27 17:02:11 -0600

    added a link to my email address at the bottom of my ContactMe page

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -10,6 +10,9 @@ export default function ContactMe() {
         <Header />
         <h1 className="pageTitle">Contact Me</h1>
         <Contact />
+        <div style={{textAlign:"center"}}>
+          If you have any questions about this Project or my Repository, I can be reached at <a href="mailto:brad@bradburr.com">brad@bradburr.com</a>
+        </div>
       </div>
       <Footer />
     </div>
